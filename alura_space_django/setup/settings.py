6a4d8a88b117494a583df44d6c7ab9fe8f1eb075
Caio <caio.terto@souunit.com.br>
2025-03-14 14:47:39 -0300

    Aplicação Django

--- alura_space_django/setup/settings.py
+++ alura_space_django/setup/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'galeria',
 ]
 
 MIDDLEWARE = [
